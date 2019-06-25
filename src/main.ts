f8c7854c3b694cb0139f3babe80a8d8b493e8500
Mehdi Ichkarrane <m.ichkarrane@expaceo.com>
2019-06-25 22:38:18 +0200

    Add font-awesome

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+library.add(faTrash, faPencilAlt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
